f4d5715bdb4abce0d37de967d53224716b8eb126
Gabriel C. Troia <catalin.troia@gmail.com>
2023-01-05 17:52:50 +0200

    Add a checker to the given error when setting the stack

--- src/util.ts
+++ src/util.ts
@@ -6,15 +6,17 @@ export const delay = (ms: number = 10) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
-}
+};
 
 export const traceAsyncErrors = async <T>(fn: () => Promise<T>) => {
   var startStack = new Error().stack || '';
   try {
     return await fn();
   } catch (error) {
-    error.stack = error.stack + "\n" +
-      startStack.substring(startStack.indexOf("\n") + 1);
+    if (error instanceof Error) {
+      error.stack =
+        error.stack + '\n' + startStack.substring(startStack.indexOf('\n') + 1);
+    }
     throw error;
   }
-}
+};
